2de674d895785545527a05863f845c53fb8d8831
Razik <razik3407@gmail.com>
2026-01-07 21:14:38 +0530

    removed unwanted

--- src/screens/elementDefault/sections/ServicesSection/ServiceSection.tsx
+++ src/screens/elementDefault/sections/ServicesSection/ServiceSection.tsx
@@ -1,6 +1,4 @@
-import React from "react";
 import { Card, CardContent } from "../../../../components/ui/card";
-import { Separator } from "../../../../components/ui/seperator";
 
 const services = [
   {
